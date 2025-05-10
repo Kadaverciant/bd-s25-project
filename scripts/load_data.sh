61f83e85a411ba608cdd90659ce9d98e7582eb1a
Vsevolod Klyushev <kadaverciant@gmail.com>
2025-05-10 16:18:58 +0300

    Download data

--- scripts/load_data.sh
+++ scripts/load_data.sh
@@ -3,6 +3,6 @@ url="https://disk.yandex.ru/d/lDwhLw7wAAm6hA"
 wget "$(yadisk-direct $url)" -O data/data.zip
 
 unzip data/data.zip -d data/
-#cp data/toy-data/*.csv data/
-#rm -rf data/toy-data
-#rm data/data.zip
+cp data/bd-data/*.csv data/
+rm -rf data/bd-data
+rm data/data.zip
