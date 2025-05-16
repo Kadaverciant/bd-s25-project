79377470620d9f1bdda4d1af4190aa9f6fdceca4
Vsevolod Klyushev <kadaverciant@gmail.com>
2025-05-17 00:12:44 +0300

    send data to hdfs

--- scripts/build_hive.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PROJECT_FOLDER="/user/team1/project"
-WAREHOUSE_FOLDER="$PROJECT_FOLDER/warehouse"
-
-password=$(head -n 1 secrets/.psql.pass)
-beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team1 -p $password -f sql/craete_tables.hql
