5184cac729dbbd532666db0532838a574dacd1b7
adbedlam <nyaneev@yandex.ru>
2025-05-16 23:16:32 +0300

    add q1 insight

--- scripts/get_insight.sh
+++ scripts/get_insight.sh
@@ -4,4 +4,4 @@ PROJECT_FOLDER="/user/team1/project"
 password=$(head -n 1 secrets/.psql.pass)
 
 # q1 insight
-beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team1 -p "$password" -f sql/partition.hql
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team1 -p "$password" -f sql/q1.hql
