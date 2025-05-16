b43d49ac20cee15c5ee4f643368669750a9281d7
Vsevolod Klyushev <kadaverciant@gmail.com>
2025-05-16 18:46:13 +0300

    send data to hdfs

--- scripts/send_to_hdfs.sh
+++ scripts/send_to_hdfs.sh
@@ -1,7 +1,7 @@
 password=$(head -n 1 secrets/.psql.pass)
 
-$PROJECT_FOLDER="./project"
-$WAREHOUSE_FOLDER="$PROJECT_FOLDER/warehouse"
+PROJECT_FOLDER="/user/team1/project"
+WAREHOUSE_FOLDER="$PROJECT_FOLDER/warehouse"
 
 # Check if the folder exists
 hdfs dfs -test -d "$PROJECT_FOLDER"
@@ -9,7 +9,7 @@ hdfs dfs -test -d "$PROJECT_FOLDER"
 # Check the exit status of the previous command
 if [ $? -eq 0 ]; then
     echo "Project folder $PROJECT_FOLDER exists in HDFS. Start cleaning."
-    hdfs dfs -rm -rf "$PROJECT_FOLDER"
+    hdfs dfs -rm -r -f "$PROJECT_FOLDER"
     echo "Cleaned"
 else
     echo "Folder $PROJECT_FOLDER does not exist in HDFS"
@@ -22,3 +22,7 @@ hdfs dfs -chmod -R 755 "$PROJECT_FOLDER"
 
 echo "Creating warehouse folder: $WAREHOUSE_FOLDER"
 hdfs dfs -mkdir -p "$WAREHOUSE_FOLDER"
+
+hdfs dfs -chmod 755 "$WAREHOUSE_FOLDER"
+
+echo "Folder structure created successfully"
