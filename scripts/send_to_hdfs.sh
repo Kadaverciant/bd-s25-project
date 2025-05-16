3f9a2e2fe98289bc6ff8691bd5dd87c4b7ac53eb
Vsevolod Klyushev <kadaverciant@gmail.com>
2025-05-16 19:01:23 +0300

    send data to hdfs

--- scripts/send_to_hdfs.sh
+++ scripts/send_to_hdfs.sh
@@ -2,6 +2,10 @@ password=$(head -n 1 secrets/.psql.pass)
 
 PROJECT_FOLDER="/user/team1/project"
 WAREHOUSE_FOLDER="$PROJECT_FOLDER/warehouse"
+AVRO_SNAPPY_FOLDER="$WAREHOUSE_FOLDER/avro_snappy"
+PARQUET_SNAPPY_FOLDER="$WAREHOUSE_FOLDER/parquet_snappy"
+AVRO_GZIP_FOLDER="$WAREHOUSE_FOLDER/avro_gzip"
+PARQUET_GZIP_FOLDER="$WAREHOUSE_FOLDER/parquet_gzip"
 
 # Check if the folder exists
 hdfs dfs -test -d "$PROJECT_FOLDER"
@@ -16,13 +20,43 @@ else
 fi
 
 echo "Creating project folder: $PROJECT_FOLDER"
-hdfs dfs -mkdir -p "$PROJECT_FOLDER"
 
+hdfs dfs -mkdir -p "$PROJECT_FOLDER"
 hdfs dfs -chmod -R 755 "$PROJECT_FOLDER"
 
 echo "Creating warehouse folder: $WAREHOUSE_FOLDER"
-hdfs dfs -mkdir -p "$WAREHOUSE_FOLDER"
 
+hdfs dfs -mkdir -p "$WAREHOUSE_FOLDER"
 hdfs dfs -chmod 755 "$WAREHOUSE_FOLDER"
 
+echo "Creating warehouse folders (avro_snappy, parquet_snappy, avro_gzip, parquet_gzip): $WAREHOUSE_FOLDER"
+
+hdfs dfs -mkdir -p "$AVRO_SNAPPY_FOLDER"
+hdfs dfs -chmod 755 "$AVRO_SNAPPY_FOLDER"
+hdfs dfs -mkdir -p "$PARQUET_SNAPPY_FOLDER"
+hdfs dfs -chmod 755 "$PARQUET_SNAPPY_FOLDER"
+hdfs dfs -mkdir -p "$AVRO_GZIP_FOLDER"
+hdfs dfs -chmod 755 "$AVRO_GZIP_FOLDER"
+hdfs dfs -mkdir -p "$PARQUET_GZIP_FOLDER"
+hdfs dfs -chmod 755 "$PARQUET_GZIP_FOLDER"
+
 echo "Folder structure created successfully"
+
+start_time=$(date +%s)
+
+echo "Start loading AVRO + SNAPPY"
+sqoop import \
+  --connect "jdbc:postgresql://hadoop-04.uni.innopolis.ru/team1_projectdb" \
+  --username team1\
+  --password "$password" \
+  --compression-codec=snappy \
+  --compress \
+  --as-avrodatafile \
+  --warehouse-dir=$AVRO_SNAPPY_FOLDER \
+  --m 1 \
+  --table records
+
+end_time=$(date +%s)
+execution_time=$((end_time - start_time))
+
+echo "Sqoop import completed in $execution_time seconds"
