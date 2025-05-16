4e47fdcdfab3d531816cf16b218465d7d13114a7
Vsevolod Klyushev <kadaverciant@gmail.com>
2025-05-16 19:49:23 +0300

    send data to hdfs

--- scripts/build_hive.sh
+++ scripts/build_hive.sh
@@ -7,4 +7,5 @@ PARQUET_SNAPPY_FOLDER="$WAREHOUSE_FOLDER/parquet_snappy"
 AVRO_GZIP_FOLDER="$WAREHOUSE_FOLDER/avro_gzip"
 PARQUET_GZIP_FOLDER="$WAREHOUSE_FOLDER/parquet_gzip"
 
-hive -f sql/craete_tables.hql
+password=$(head -n 1 secrets/.psql.pass)
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team1 -p $password -f sql/craete_tables.hql
