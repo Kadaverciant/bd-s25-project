583edf2ff489ebec23f4fec04a218ffe1f485ed7
adbedlam <nyaneev@yandex.ru>
2025-05-17 14:55:44 +0300

    add q2 insight

--- scripts/get_insight.sh
+++ scripts/get_insight.sh
@@ -4,4 +4,9 @@ PROJECT_FOLDER="/user/team1/project"
 password=$(head -n 1 secrets/.psql.pass)
 
 # q1 insight
+echo "First insight"
 beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team1 -p "$password" -f sql/q1.hql
+
+# q2 insight
+echo "Second insight"
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team1 -p "$password" -f sql/q2.hql
