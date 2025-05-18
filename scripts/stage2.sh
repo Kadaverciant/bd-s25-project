e38a961de01bc27431f00cff5cfa9d8760d30e2a
Vsevolod Klyushev <kadaverciant@gmail.com>
2025-05-18 19:34:04 +0300

    formating

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+password=$(head -n 1 secrets/.psql.pass)
 echo "Start building HIVE table"
 beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team1 -p $password -f sql/create_tables.hql
 
