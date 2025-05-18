4cd6aeebcbceec125c9a4ddddc0ff4f35bf0b782
Vsevolod Klyushev <kadaverciant@gmail.com>
2025-05-18 22:23:04 +0300

    formating

--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -4,9 +4,9 @@ password=$(head -n 1 secrets/.psql.pass)
 
 spark-submit --master yarn ml/app.py
 
-hdfs dfs -cat project/output/model1_predictions.csv/*.csv > output/model1_predictions.csv
+hdfs dfs -cat project/output/model1_predictions/*.csv > output/model1_predictions.csv
 
-hdfs dfs -cat project/output/model2_predictions.csv/*.csv > output/model2_predictions.csv
+hdfs dfs -cat project/output/model2_predictions/*.csv > output/model2_predictions.csv
 
 hdfs dfs -cat project/output/evaluation.csv/*.csv > output/evaluation.csv
 
