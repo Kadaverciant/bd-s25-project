3852c1b56e81c1553ce7889c0afb7caadf7b3d66
adbedlam <nyaneev@yandex.ru>
2025-05-19 22:02:20 +0300

    add main.sh, fix stage3.sh

--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -3,13 +3,18 @@
 password=$(head -n 1 secrets/.psql.pass)
 
 spark-submit --master yarn ml/app.py
-#spark-submit  ml/app.py
 
 hdfs dfs -cat project/output/model1_predictions/*.csv > output/model1_predictions.csv
 
 hdfs dfs -cat project/output/model2_predictions/*.csv > output/model2_predictions.csv
 
 hdfs dfs -cat project/output/evaluation.csv/*.csv > output/evaluation.csv
+
+mkdir ./models
+
+hdfs dfs -get project/models/model1 models/model1
+
+hdfs dfs -get project/models/model2 models/model2
 
 echo "Run formatter"
 sh ./scripts/format.sh
