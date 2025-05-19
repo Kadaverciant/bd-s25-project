218ef0d7be119891457c7d22d196396238330ab4
Vsevolod Klyushev <kadaverciant@gmail.com>
2025-05-20 01:44:45 +0300

    Update stage3

--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -16,6 +16,17 @@ hdfs dfs -get project/models/model1 models/model1
 
 hdfs dfs -get project/models/model2 models/model2
 
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team1 -p "$password" -e "
+  INSERT OVERWRITE DIRECTORY '/user/team1/project/output/grid_search_results'
+  ROW FORMAT DELIMITED
+  FIELDS TERMINATED BY ','
+  STORED AS TEXTFILE
+  SELECT * FROM team1_projectdb.grid_search_results;"
+
+echo "model_name,RMSE,MAE" > output/grid_search_results.csv
+hdfs dfs -cat ./project/output/grid_search_results/* >> output/grid_search_results.csv
+head -n 10 output/grid_search_results.csv
+
 echo "Run formatter"
 sh ./scripts/format.sh
 
