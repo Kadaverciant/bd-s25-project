8dfaddf0e983d16dd258c821d39cc5537d4733e3
adbedlam <nyaneev@yandex.ru>
2025-05-20 20:13:12 +0300

    small fixes

--- scripts/send_to_hdfs.sh
+++ scripts/send_to_hdfs.sh
@@ -41,27 +41,27 @@ show_hdfs_size() {
 start_time=$(date +%s)
 
 echo "Start loading"
-#sqoop import \
-#  --connect "jdbc:postgresql://hadoop-04.uni.innopolis.ru/team1_projectdb" \
-#  --username team1\
-#  --password "$password" \
-#  --as-parquetfile \
-#  --warehouse-dir=$WAREHOUSE_FOLDER \
-#  --m 1 \
-#  --outdir "$(pwd)/output" \
-#  --table records
-
 sqoop import \
   --connect "jdbc:postgresql://hadoop-04.uni.innopolis.ru/team1_projectdb" \
   --username team1\
   --password "$password" \
-  --compression-codec=snappy \
-  --compress \
-  --as-avrodatafile \
+  --as-parquetfile \
   --warehouse-dir=$WAREHOUSE_FOLDER \
   --m 1 \
   --outdir "$(pwd)/output" \
   --table records
+
+#sqoop import \
+#  --connect "jdbc:postgresql://hadoop-04.uni.innopolis.ru/team1_projectdb" \
+#  --username team1\
+#  --password "$password" \
+#  --compression-codec=snappy \
+#  --compress \
+#  --as-avrodatafile \
+#  --warehouse-dir=$WAREHOUSE_FOLDER \
+#  --m 1 \
+#  --outdir "$(pwd)/output" \
+#  --table records
 
 #sqoop import \
 #  --connect "jdbc:postgresql://hadoop-04.uni.innopolis.ru/team1_projectdb" \
@@ -78,8 +78,8 @@ sqoop import \
 end_time=$(date +%s)
 execution_time=$((end_time - start_time))
 
-hdfs dfs -mkdir -p "$WAREHOUSE_FOLDER/avsc"
-hdfs dfs -put -f output/*.avsc "$WAREHOUSE_FOLDER/avsc"
+#hdfs dfs -mkdir -p "$WAREHOUSE_FOLDER/avsc"
+#hdfs dfs -put -f output/*.avsc "$WAREHOUSE_FOLDER/avsc"
 
 echo "Sqoop import completed in $execution_time seconds"
 show_hdfs_size "$WAREHOUSE_FOLDER"
